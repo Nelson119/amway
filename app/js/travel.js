4bf23aa1c71bdb17790df3335b28308307092123
妞森 <niusen@niusentekiAir.mshome.net>
2016-09-12 11:18:48 +0800

    kv image update

--- app/js/travel.js
+++ app/js/travel.js
@@ -16,31 +16,6 @@ app.partial.travel = function(){
 			$('figcaption span:eq(1)', travel).html(perc);
 			$('svg circle', travel).css('stroke-dashoffset', perc);
 			$('svg circle', travel.siblings()).css('stroke-dashoffset', 0);
-			// if(perc === 0){
-			// 	return;
-			// }
-
-
-			// $('figcaption span:eq(1)', travel.siblings()).html(0);
-
-			// var tl = new TimelineMax({
-			// 	onComplete: function(){
-			// 		var t = $('figcaption span:eq(1)', travel).html() * 100;
-			// 		if(t < perc * 100){
-			// 			tl.seek(0);
-			// 			tl.play();
-			// 		}else{
-			// 			 $('figcaption span:eq(1)', travel).html(perc);
-			// 		}
-			// 	}
-			// });
-
-			// tl.addPause(1 / perc / 10000, next);
-			// function next(){
-			// 	var t = Math.round($('figcaption span:eq(1)', travel).html() * 100);
-			// 	var p = ((t + 53) / 100).toString().replace(/^(.*\.\d{0,2})\d*/i, '$1');
-			// 	$('figcaption span:eq(1)', travel).html(p);
-			// }
 		} else{
 			travel.removeClass('on');
 			$('svg circle', travel).css('stroke-dashoffset', 0);
